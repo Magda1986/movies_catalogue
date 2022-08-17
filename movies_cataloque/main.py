c404700f9b46428203aa8db453388836095bfd22
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2022-08-17 23:20:28 +0200

    Update

--- movies_cataloque/main.py
+++ movies_cataloque/main.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
 
 app = Flask(__name__)
 
-# strona gwna aplikacji, lista movies zawiera filmy w naszej bibliotece
+list_type = {
+        "Popular": "popular", "Top Rated ": "top_rated", "Upcoming": "upcoming", "Now playing": "now_playing"
+    }
+
+# strona gwna aplikacji, lista movies zawiera filmy w naszej bibliot
+# @app.route("/")
+# def homepage():
+#     #movies = ["Top Gun", "Tom Gun Maverick", "Mission Impossible","Vanilla Sky", "Jack Reacher", "Rain Man", "Ludzie honoru", "Jerry Maguire"]
+#     movies = tmdb_client.get_movies(how_many=8)
+#     return render_template("homepage.html", movies=movies)
+
 @app.route("/")
-def homepage():
-    #movies = ["Top Gun", "Tom Gun Maverick", "Mission Impossible","Vanilla Sky", "Jack Reacher", "Rain Man", "Ludzie honoru", "Jerry Maguire"]
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+def listpage():
+    selected_list = request.args.get('list_type', "popular")
+    if selected_list not in list_type.values():
+        selected_list = "popular"
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, list_types=list_type.items(), current_list=selected_list)
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    details["cast"] = tmdb_client.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=details)
 
 
 # Poniszy zapis umozliwia uruchamić aplikację bezpośrednio w edytorze, klikając przycisk “Play” w prawym górnym rogu okna
--- movies_cataloque/tmdb_client.py
+++ movies_cataloque/tmdb_client.py
@@ -1,13 +1,25 @@
+#from nturl2path import url2pathname
 import requests
 
 # funkcja zwraca pełną listę popularnych filmów, "Authorization" - token autoryzacyjny
-def get_popular_movies():
+def get_popular_movies(list_type):
     api_key = "6fff7883f182efb5650e7d40ba60ee3b"
-    endpoint = f"https://api.themoviedb.org/3/movie/popular"
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
     url = endpoint + "?api_key=" + api_key
     # api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmZmNzg4M2YxODJlZmI1NjUwZTdkNDBiYTYwZWUzYiIsInN1YiI6IjYyZGU2MzdhZWE4NGM3MTRlNTg3OTljYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UM4-UJjMj00nFCFVOnhxjONbpX8LSffMqbR9VD1SLG8"
     # headers = {"Authorization": f"Bearer {api_token}"}
     response = requests.get(url)
+    return response.json()
+
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmZmNzg4M2YxODJlZmI1NjUwZTdkNDBiYTYwZWUzYiIsInN1YiI6IjYyZGU2MzdhZWE4NGM3MTRlNTg3OTljYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UM4-UJjMj00nFCFVOnhxjONbpX8LSffMqbR9VD1SLG8"
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
     return response.json()
 
 # funkcja zwraca kompletny/działający adres URL do obrazka, poster_api_path-ścieżka do plakatu , size-rozmiar
@@ -17,16 +29,48 @@ def get_poster_url(poster_api_path, size="w342"):
 
 # funkcja modyfikuje sownik zawierający film
 def get_movie_info(movie):
-    return {"title" : movie["title"],
-    "poster_url" : get_poster_url(movie["poster_path"])}
+    return {"id" : movie["id"],
+            "title" : movie["title"],
+            "poster_url" : get_poster_url(movie["poster_path"])}
 
-# Funkcja pobiera filmy i traktuje powyszą funkcją
-def get_movies(how_many=20):
-    all_movies = get_popular_movies()["results"][:how_many]
+# Funkcja pobiera filmy - 20 i traktuje powyszą funkcją
+def get_movies(how_many=20, list_type="popular"):
+    all_movies = get_popular_movies(list_type)["results"][:how_many]
     return [get_movie_info(movie) for movie in all_movies]
 
 #print(get_movies())
 
+def get_api_from_tmdb(endpoint):
+    url_adres = f"https://api.themoviedb.org/3/{endpoint}"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmZmNzg4M2YxODJlZmI1NjUwZTdkNDBiYTYwZWUzYiIsInN1YiI6IjYyZGU2MzdhZWE4NGM3MTRlNTg3OTljYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UM4-UJjMj00nFCFVOnhxjONbpX8LSffMqbR9VD1SLG8"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(url_adres, headers=headers)
+    response.raise_for_status()
+    return response.json()
 
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmZmNzg4M2YxODJlZmI1NjUwZTdkNDBiYTYwZWUzYiIsInN1YiI6IjYyZGU2MzdhZWE4NGM3MTRlNTg3OTljYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UM4-UJjMj00nFCFVOnhxjONbpX8LSffMqbR9VD1SLG8"
 
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response_json = response.json()
+    response_json["poster_url"] = get_poster_url(response_json["backdrop_path"], size="w780")
+    return response_json
+
+
+def get_single_movie_cast(movie_id, how_many=4):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response_json = response.json()["cast"][:how_many]
+    for actor in response_json:
+        actor["profile_url"] = get_poster_url(actor["profile_path"], size="w185")
+    return response_json    
